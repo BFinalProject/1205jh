96275d354d285f6b0f1471b4487cf19e11f739ce
leeexiu <leeexiu@gmail.com>
2022-12-05 15:48:22 +0900

    아이디비밀번호찾기페이지추가

--- src/components/vues/login.vue
+++ src/components/vues/login.vue
@@ -4,7 +4,6 @@
     <div class="container1" id="container1">
       <div class="form-container1 sign-up-container1">
         <form action="#">
-          <br />
           <h1>회원가입하기</h1>
           <div class="social-container1"></div>
 
@@ -23,7 +22,7 @@
                   padding-top: 5px;
                   padding-bottom: 5px;
                 "
-                v-model="user_gender"
+                v-model="signup_gender"
               />
               <label class="form-check-label" for="inlineRadio1">남자</label>
             </div>
@@ -40,22 +39,22 @@
                   padding-top: 5px;
                   padding-bottom: 5px;
                 "
-                v-model="user_gender"
+                v-model="signup_gender"
               />
               <label class="form-check-label" for="inlineRadio2">여자</label>
             </div>
           </div>
 
-          <input type="text" placeholder="이름" v-model="user_name" /><br />
+          <input type="text" placeholder="이름" v-model="signup_name" /><br />
 
-          <input type="email" placeholder="아이디" v-model="user_id" /><br />
+          <input type="email" placeholder="아이디" v-model="signup_id" /><br />
 
           <input
             type="password"
             id="password1"
             onchange="passwordreconfirm()"
             placeholder="비밀번호"
-            v-model="user_pw"
+            v-model="signup_pw"
           /><br />
           <span id="pwConfirm" style="display: none"
             >입력한 글자가 6글자 이상이어야 합니다.</span
@@ -77,11 +76,19 @@
             >비밀번호가 일치 합니다.</span
           >
 
-          <input type="email" placeholder="이메일" v-model="user_mail" /><br />
-          <input type="tel" placeholder="휴대폰번호" v-model="user_phonenum" />
+          <input
+            type="email"
+            placeholder="이메일"
+            v-model="signup_mail"
+          /><br />
+          <input
+            type="tel"
+            placeholder="휴대폰번호"
+            v-model="signup_phonenum"
+          />
           <br />
 
-          <span>체크한 이름: {{ user_gender }}</span>
+          <span>체크한 이름: {{ signup_gender }}</span>
 
           <button type="button" class="button1" @click="usersignup()">
             회원가입하기
@@ -92,17 +99,18 @@
         <form action="#">
           <h1>로그인</h1>
           <br />
-          <input type="email" placeholder="아이디" />
+          <input type="email" placeholder="아이디" v-model="user_id" />
           <br />
-          <input type="password" placeholder="비밀번호" />
-          <a href="/findidpw">아이디 혹은 비밀번호를 잊어버리셨나요?</a>
+          <input type="password" placeholder="비밀번호" v-model="user_pw" />
+          <a href="#">아이디 혹은 비밀번호를 잊어버리셨나요?</a>
 
           <div class="login">
             <button
               type="button"
               class="button1"
               style="margin-bottom: 10px"
-              onclick="location.href='/main2'"
+              onclick="location.href='#'"
+              @click="userlogin()"
             >
               로그인
             </button>
@@ -118,16 +126,14 @@
       <div class="overlay-container1">
         <div class="overlay">
           <div class="overlay-panel overlay-left">
-            <h1>환영합니다!</h1>
+            <h1>Welcome Back!</h1>
             <p>
-              우리 서비스를 이용해주셔서 감사합니다
+              To keep connected with us please login with your personal info
             </p>
-            <button type="button" class="button1" id="signIn">
-              로그인하기
-            </button>
+            <button type="button" class="button1" id="signIn">Sign In</button>
           </div>
           <div class="overlay-panel overlay-right">
-            <h1>만나서 반갑습니다!</h1>
+            <h1>Hello, Friend!</h1>
             <p>아이디가 없으신가요?</p>
             <button type="button" class="button1" id="signUp">
               회원가입하기
@@ -151,12 +157,14 @@ export default {
   data: function() {
     return {
       user_signup_Information: [],
-      user_gender: "",
-      user_name: "",
       user_id: "",
       user_pw: "",
-      user_mail: "",
-      user_phonenum: ""
+      signup_gender: "",
+      signup_name: "",
+      signup_id: "",
+      signup_pw: "",
+      signup_mail: "",
+      signup_phonenum: ""
       // userList:[],
       // updatepassword:[]
     };
@@ -214,24 +222,44 @@ export default {
       //         });
       //     },
     },
-    //로그인 펑션
+    //회원가입 함수
     usersignup: function() {
       let vm = this;
       // let board_id = vm.$route.query.boardId;
-      console.log(vm.user_gender);
-      console.log(vm.user_name);
-      console.log(vm.user_id);
-      console.log(vm.user_pw);
-      console.log(vm.user_mail);
-      console.log(vm.user_phonenum);
+      console.log(vm.signup_gender);
+      console.log(vm.signup_name);
+      console.log(vm.signup_id);
+      console.log(vm.signup_pw);
+      console.log(vm.signup_mail);
+      console.log(vm.signup_phonenum);
 
       let send = {
-        usergender: vm.user_gender,
-        username: vm.user_name,
+        signupgender: vm.signup_gender,
+        signupname: vm.signup_name,
+        signupid: vm.signup_id,
+        signuppw: vm.signup_pw,
+        signupmail: vm.signup_mail,
+        signupphonenum: vm.signup_phonenum
+      };
+
+      let url = "/user/signup";
+
+      console.log(url);
+
+      Axios.post(url, send).then(function(response) {
+        console.log("stsetsetset");
+      });
+    },
+    //로그인 함수
+    userlogin: function() {
+      let vm = this;
+      // let board_id = vm.$route.query.boardId;
+      console.log(vm.user_id);
+      console.log(vm.user_pw);
+
+      let send = {
         userid: vm.user_id,
-        userpw: vm.user_pw,
-        usermail: vm.user_mail,
-        userphonenum: vm.user_phonenum
+        userpw: vm.user_pw
       };
 
       let url = "/user/login";
